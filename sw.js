d99480bc80cdbd4632df397d09034aa2b0dddba2
Josafá <josafart.net@gmail.com>
2026-04-01 22:55:43 -0300

    commit_V4_confirmacao_usuario_cadastrado

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Time Leonel - Service Worker
 // ⚠️ INCREMENTAR ESTA VERSÃO a cada deploy para forçar atualização no celular
-const CACHE_VERSION = 'time-leonel-v8';
+const CACHE_VERSION = 'time-leonel-v9';
 
 const ASSETS = [
   './',
